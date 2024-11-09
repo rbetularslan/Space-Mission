d4e0d131518ef3d3f0735d72959aaf258a811da3
Raziye <betularslanbb@gmail.com>
2024-11-09 23:09:06 +0300

    Initial Commit

---
+++ Space MissionUITests/Space_MissionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Space_MissionUITestsLaunchTests.swift
+//  Space MissionUITests
+//
+//  Created by Raziye Betül Arslan on 9.11.2024.
+//
+
+import XCTest
+
+final class Space_MissionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
